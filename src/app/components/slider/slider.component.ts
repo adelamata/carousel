f7745fa3075ea2e4ef0325da6a12572775124ddf
adelamata <antoniodelamatalopez@hotmail.es>
2021-10-04 16:32:28 +0200

    1 file modified

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit } from '@angular/core';
+import { SlideComponent } from './components/slide/slide.component';
 
 @Component({
   selector: 'slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
 })
-export class SliderComponent {}
+export class SliderComponent implements AfterContentInit {
+  @ContentChildren(SlideComponent) slides: SlideComponent[];
+
+  ngAfterContentInit() {
+    console.log(this.slides);
+  }
+}
