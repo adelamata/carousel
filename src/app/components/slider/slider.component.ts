c0e5ba4d4da6dbffe21f627efe56aa1bae9fe039
adelamata <antoniodelamatalopez@hotmail.es>
2021-10-04 16:45:37 +0200

    1 file modified

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,4 +1,10 @@
-import { Component, AfterContentInit, ContentChildren } from '@angular/core';
+import {
+  Component,
+  AfterContentInit,
+  ContentChildren,
+  HostListener,
+  HostBinding,
+} from '@angular/core';
 import { SlideComponent } from './components/slide/slide.component';
 
 @Component({
@@ -9,12 +15,17 @@ import { SlideComponent } from './components/slide/slide.component';
 export class SliderComponent implements AfterContentInit {
   @ContentChildren(SlideComponent) slides: SlideComponent[];
 
+  @HostBinding('class') class;
+
+  @HostListener('click') clicked() {
+    console.log ('click');
+    this.class = 'transition-effect';
+  }
+
   currentSlide = 1;
 
   ngAfterContentInit() {
     console.log(this.slides);
-    this.slides.forEach(
-      (slide, index) => slide.order = index + 1
-    );
+    this.slides.forEach((slide, index) => (slide.order = index + 1));
   }
 }
